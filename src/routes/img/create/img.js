39a55019b2bd957963759bd057446fa4aba9e949
ghtx280 <an280502@gmail.com>
2024-03-26 22:19:34 +0200

    ffdfd

--- src/routes/img/create/img.js
+++ src/routes/img/create/img.js
@@ -1,11 +1,8 @@
-
-
 export async function img(p) {
     p.w = +p.w || this.config.width;
     p.h = +p.h || this.config.height;
     p.x = +p.x || this.config.height / 2;
     p.y = +p.y || this.config.height / 2;
-
 
     this.ctx.save();
 
@@ -13,19 +10,20 @@ export async function img(p) {
     this.ctx.rotate(Math.PI / (180 / (+p.r || 0)));
     // this.ctx.translate(-p.w / 2, -p.h / 2);
     let region = new Path2D();
-    region.roundRect(
-        (-p.w / 2), (-p.h / 2),
-        p.w,
-        p.h, 
-        +p.rd || 0
-    );
+    region.roundRect(-p.w / 2, -p.h / 2, p.w, p.h, +p.rd || 0);
     this.ctx.clip(region, 'evenodd');
 
-    
+    if (!p.data?.$elem) {
+        p.data = {
+            $elem: await loadImage(p.data),
+            $name: p.data,
+        };
+    }
 
     this.ctx.drawImage(
-        p.data?.$name ? p.data.$elem : await loadImage(p.data),
-        (-p.w / 2), (-p.h / 2),
+        p.data?.$elem,
+        -p.w / 2,
+        -p.h / 2,
         // 0, 0,
         p.w,
         p.h
